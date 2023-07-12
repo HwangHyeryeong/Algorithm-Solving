58942b9915754cd2d3f458ee5712af0103f6b769
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-12 09:07:22 +0900

    [Silver II] Title: 스택 수열, Time: 2332 ms, Memory: 127300 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] target = new int[n];
+		for(int i = 0; i < n; i++)
+			target[i] = sc.nextInt();
+		
+		Stack<Integer> s = new Stack<>();
+		ArrayList<Character> ans = new ArrayList<>();
+		int cur = 0;
+		for(int i = 1; i <= n; i++) {
+			s.push(i);
+			ans.add('+');
+			while(!s.isEmpty() && s.peek() == target[cur]) {
+				ans.add('-');
+				s.pop();
+				cur++;
+			}
+		}
+		while(!s.isEmpty() && s.peek() == target[cur]) {
+			ans.add('-');
+			s.pop();
+			cur++;
+		}
+		
+		if(s.size() == 0)
+			for(char c : ans)
+				System.out.println(c);
+		else
+			System.out.println("NO");
+	}
+}
