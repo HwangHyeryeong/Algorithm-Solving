71ee049def7e736c233ca472e2bfdbfe225a0a6a
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-12 16:23:04 +0900

    [Bronze II] Title: 수 정렬하기, Time: 356 ms, Memory: 22156 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -10,11 +10,14 @@ public class Main{
 		for(int i = 0; i < n; i++)
 			arr[i] = sc.nextInt();
 
-		for(int i = 0; i < n - 1; i++)
-			for(int j = 0; j < n - i - 1; j++) {
-				if(arr[j] > arr[j+1])
-					swap(arr, j, j+1);
+		for(int i = 0; i < n - 1; i++) {
+			int min = i;
+			for(int j = i + 1; j < n; j++) {
+				if(arr[min] > arr[j])
+					min = j;
 			}
+			swap(arr, i, min);
+		}
 
 		for(int i : arr)
 			System.out.println(i);
