8abe124de0082e04d377afc2ea16a9e57a457397
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-13 14:41:01 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
 		for(int i = 0; i < n; i++)
 			time[i] = Integer.parseInt(st.nextToken());
 		
-		mergeSort(time, 0, n-1);
+		Arrays.sort(time);
 		
 		int ans = 0;
 		for(int i = 0, j = n; i < n; i++, j--) {
@@ -22,33 +23,5 @@ public class Main {
 		}
 		
 		System.out.println(ans);		
-	}
-	
-	static void mergeSort(int[] arr, int start, int end) {
-		if(start >= end) return;
-		
-		int mid = (start + end) / 2;
-		mergeSort(arr, start, mid);
-		mergeSort(arr, mid+1, end);
-		
-		merge(arr, start, end, mid);
-	}
-	
-	static void merge(int[] arr, int start, int end, int mid) {
-		int[] sorted = new int[end - start + 1];
-		int pl = start, pr = mid + 1, pc = 0;
-		
-		while(pl <= mid && pr <= end) {
-			sorted[pc++] = (arr[pl] < arr[pr])? arr[pl++] : arr[pr++];
-		}
-		
-		while(pl <= mid)
-			sorted[pc++] = arr[pl++];
-		
-		while(pr <= end)
-			sorted[pc++] = arr[pr++];
-		
-		for(int i = start, j = 0; i <= end; i++, j++)
-			arr[i] = sorted[j];
 	}
 }
