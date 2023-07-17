d07cca6cf1bb890125eb892e3c94cb5096e2419d
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-17 17:16:01 +0900

    [Gold V] Title: 트리, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    	static int n, remove, ans = 0;
+	static ArrayList<Integer>[] tree;
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		tree = new ArrayList[n];
+		visited = new boolean[n];
+		for(int i = 0; i < n; i++)
+			tree[i] = new ArrayList<>();
+		
+		int root = -1;
+		for(int i = 0; i < n; i++) {
+			int parent = sc.nextInt();
+			if(parent == -1) {
+				root = i;
+				continue;
+			}
+			tree[parent].add(i);
+		}
+		remove = sc.nextInt();
+		
+		dfs(root);
+		
+		System.out.println(ans);
+	}
+	
+	static void dfs(int cur) {
+		if(cur == remove) return;
+		
+		if(tree[cur].size() == 0) {
+			ans++;
+			return;
+		}
+		
+		for(int next : tree[cur]) {
+			if(!visited[next]) {
+				visited[next] = true;
+				if(next == remove ) {
+					if(tree[cur].size() == 1) ans++;
+					continue;
+				}
+				else 
+					dfs(next);
+			}
+		}
+	}
+}
