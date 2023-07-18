e4915df06b9e1c092634efecbcee5164c60a8e4e
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-18 14:10:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,65 +1,63 @@
 #include <iostream>
-#include <stack>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
+int n, m, v;
 vector<int> graph[1001];
+bool visitedD[1001] = { 0 };
+bool visitedB[1001] = { 0 };
 
+void dfs(int s) {
+	if (visitedD[s])
+		return;
+	
+	visitedD[s] = true;
+	cout << s << " ";
 
-void bfs(int start) {
-	bool visited[1001] = { 0 };
+	for (int i = 0; i < graph[s].size(); i++) {
+		if (!visitedD[graph[s][i]])
+			dfs(graph[s][i]);
+	}
+}
+
+void bfs(int s) {
 	queue<int> q;
 
-	q.push(start);
-	visited[start] = true;
+	visitedB[s] = true;
+	q.push(s);
 	while (!q.empty()) {
-		int v = q.front(); q.pop();
-		cout << v << ' ';
-		for (int i : graph[v]) {
-			if (!visited[i]) {
-				q.push(i);
-				visited[i] = true;
+		int cur = q.front(); q.pop();
+		cout << cur << " ";
+
+		for (int i = 0; i < graph[cur].size(); i++) {
+			if (!visitedB[graph[cur][i]]) {
+				visitedB[graph[cur][i]] = true;
+				q.push(graph[cur][i]);
 			}
 		}
 	}
 	cout << '\n';
 }
 
-void dfs(int start, vector<bool> &visited) {
-	if (visited[start]) return;
-	
-	visited[start] = true;
-	cout << start << ' ';
-
-	for (int i = 0; i < graph[start].size(); i++) {
-		int x = graph[start][i];
-		if (!visited[x])
-			dfs(x, visited);
-	}
-}
-
 int main() {
 	std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
 
-	int n, m, s;
-	cin >> n >> m >> s;
-
-	for (int i = 0; i < m; i++) {
-		int v1, v2; cin >> v1 >> v2;
-		graph[v1].push_back(v2);
-		graph[v2].push_back(v1);
+	cin >> n >> m >> v;
+	while (m--) {
+		int a, b; cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
 	}
 
-	//정점 번호 작은 것부터 방문하도록
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++) {
 		sort(graph[i].begin(), graph[i].end());
+	}
 
-	vector<bool> visited(n + 1);
-	dfs(s, visited);
+	dfs(v); 
 	cout << '\n';
-	bfs(s);
+	bfs(v);
 
 	return 0;
 }
