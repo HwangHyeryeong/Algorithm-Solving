cb301efbb1b2d76fe78ca92c209829fbf11c2039
HwangHyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-07-20 15:34:40 +0900

    [Silver I] Title: 회의실 배정, Time: 1316 ms, Memory: 177408 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Integer[][] meeting = new Integer[n][2];
+
+		for(int i = 0; i < n; i++) {
+			meeting[i][0] = sc.nextInt();
+			meeting[i][1] = sc.nextInt();
+		}
+
+		Arrays.sort(meeting, (a,b) -> ((a[1] != b[1]) ? a[1] - b[1] : a[0] - b[0]));
+
+		int ans = 0, end = 0;
+		for(int i = 0; i < n; i++) {
+			if(meeting[i][0] == meeting[i][1]) {
+				end = meeting[i][1];
+				ans++;
+				continue;
+			}
+			if(end <= meeting[i][0]) {
+				end = meeting[i][1];
+				ans++;
+			}
+		}
+
+		System.out.println(ans);
+		sc.close();
+	}
+}
