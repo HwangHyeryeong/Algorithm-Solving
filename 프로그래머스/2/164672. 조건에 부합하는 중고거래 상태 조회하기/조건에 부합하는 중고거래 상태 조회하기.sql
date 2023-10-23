91800f4078d268a3a97cd4dfa69d6756695dc4e1
Hwang Hyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-10-23 16:52:51 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+        case STATUS when 'SALE' then '판매중' when 'RESERVED' then '예약중' when 'DONE' then '거래완료' end as STATUS
+from USED_GOODS_BOARD 
+where created_date = '2022-10-05'
+order by board_id desc;
