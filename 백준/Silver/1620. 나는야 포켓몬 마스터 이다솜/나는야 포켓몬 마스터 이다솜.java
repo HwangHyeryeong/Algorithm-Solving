471d410d05bfb9edc1f82107ec057c1c96e3a963
Hwang Hyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2023-11-18 14:59:21 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 600 ms, Memory: 55196 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -4,22 +4,22 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer token = new StringTokenizer(reader.readLine());
 	    
+	    StringTokenizer token = new StringTokenizer(reader.readLine());
         int n = Integer.parseInt(token.nextToken());
         int m = Integer.parseInt(token.nextToken());
+        
         Map<String, String> pocketmons = new HashMap<>();
         
 		for(int i = 1; i <= n; i++){
-		     token = new StringTokenizer(reader.readLine());
-		     String name = token.nextToken();
+		     String name = reader.readLine().trim();
 		     pocketmons.put(String.valueOf(i), name);
 		     pocketmons.put(name, String.valueOf(i));
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < m; i++){
-		    String target = new StringTokenizer(reader.readLine()).nextToken();
+		    String target = reader.readLine().trim();
 		    sb.append(pocketmons.get(target)).append('\n');
 		}
 		
