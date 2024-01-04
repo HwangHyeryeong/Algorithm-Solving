c4e40a47826802c64269de828db30c5b44befd31
Hwang Hyeryeong <75305711+HwangHyeryeong@users.noreply.github.com>
2024-01-04 15:25:32 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,27 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int x;
-int num[1000001] = { 0 };
-
-void getNum(){
-    for(int i = 2; i <= x; i++){
-        int a = 9999999, b = 9999999, c = 9999999;
-        
-        if(i % 3 == 0)
-            a = num[i / 3] + 1;
-        if(i % 2 == 0)
-            b = num[i / 2] + 1;
-        c = num[i - 1] + 1;
-        num[i] = min({a, b, c});
-    }
-}
-
 int main(){
-    cin >> x;
-    getNum();
+    int x; cin >> x;
+    vector<int> dp(x + 1, 0);
     
-    cout << num[x] << "\n";
+    for(int i = 2; i <= x; i++){
+        dp[i] = dp[i - 1] + 1;
+            
+        if(i % 3 == 0)
+            dp[i] = min(dp[i / 3] + 1, dp[i]);
+        
+        if(i % 2 == 0)
+            dp[i] = min(dp[i / 2] + 1, dp[i]);
+    }
+    
+    cout << dp[x];
     
     return 0;
 }
